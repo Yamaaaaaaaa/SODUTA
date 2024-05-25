0d91c7726f1db6b82edebc2bc8aff076a0eaed08
Yamaaaaaaaa <sonaptx4869@gmail.com>
2024-05-25 09:46:27 +0700

    Update Code: MENU

---
+++ core/src/com/mygdx/game/view/InforGameScreen.java
@@ -0,0 +1,169 @@
+package com.mygdx.game.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.SpaceGame;
+
+public class InforGameScreen implements Screen {
+    // KÍCH THƯỚC NÚT
+    private int SIZE_BUTTON_WIDTH = 88;
+    private int SIZE_BUTTON_HEIGHT = 88;
+    private int SIZE_BUTTON_MUSIC_WIDTH = SIZE_BUTTON_WIDTH/2;
+    private int SIZE_BUTTON_MUSIC_HEIGHT = SIZE_BUTTON_HEIGHT/2;
+
+    // CÁC CHỨC NĂNG KHÁC
+    SpaceGame spaceGame;
+    private OrthographicCamera camera;
+
+    // ÂM THANH
+    private Music backgroundMusic;
+    private Music clickButtonMusic;
+
+    // HÌNH ẢNH
+    Texture background;
+    Texture buttonMusicOnIdle;
+    Texture buttonMusicOnHover;
+    Texture buttonMusicOffIdle;
+    Texture buttonMusicOffHover;
+    Texture howToPlay;
+    Texture buttonHomeIdle;
+    Texture buttonHomeHover;
+    Texture linkGithub;
+    // CHỨC NĂNG CHECK ÂM THANH
+    boolean checkSoundButtonMusicOffOn = false;
+    boolean isCheckSoundButtonHomeOn = false;
+    static boolean checkSoundOn = true;
+
+    public InforGameScreen(SpaceGame spaceGame){
+        this.spaceGame = spaceGame;
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Menu_Music/halloween-comedy-121626.mp3"));
+        clickButtonMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Menu_Music/clickButton.mp3"));
+        background = new Texture("button/Background.png");
+        buttonMusicOnIdle = new Texture("button/Music-On-Idle.png");
+        buttonMusicOnHover = new Texture("button/Music-On-Hover.png");
+        buttonMusicOffIdle = new Texture("button/Music-Off-Idle.png");
+        buttonMusicOffHover = new Texture("button/Music-Off-Hover.png");
+        howToPlay = new Texture("button/inforGame/HowToPlayGame.png");
+        buttonHomeHover = new Texture("button/inforGame/Home-Hover.png");
+        buttonHomeIdle = new Texture("button/inforGame/Home-Idle.png");
+        linkGithub = new Texture("button/inforGame/iconGithub.png");
+    }
+    public void show() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+        if(checkSoundOn) backgroundMusic.play();
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(1, 1, 1, 0);
+        ScreenUtils.clear(1, 1, 1, 0);
+
+        spaceGame.getBatch().begin();
+        spaceGame.getBatch().draw(background, 0, 0, 800, 800);
+        // icon Github
+        spaceGame.getBatch().draw(linkGithub,300 + SIZE_BUTTON_WIDTH + 40, 0,SIZE_BUTTON_WIDTH,SIZE_BUTTON_HEIGHT);
+        // Button go back
+        int xHome = 300 ; // tọa độ x của nút home
+        int yHome = 0; // tọa độ y của nút home
+
+        Vector3 touchPointHome = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPointHome); // Chuyển đổi tọa độ
+
+        boolean isTouchingButtonPlay = touchPointHome.x > xHome && touchPointHome.x < xHome + SIZE_BUTTON_WIDTH &&
+                touchPointHome.y > yHome && touchPointHome.y < yHome + SIZE_BUTTON_HEIGHT;
+
+        if (isTouchingButtonPlay) {
+            spaceGame.getBatch().draw(buttonHomeHover, xHome, yHome, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                backgroundMusic.pause();
+                clickButtonMusic.pause();
+                spaceGame.setScreen(new MenuScreen(spaceGame));
+            }
+            if(!isCheckSoundButtonHomeOn && checkSoundOn){
+                isCheckSoundButtonHomeOn = true;
+                clickButtonMusic.play();
+            }
+
+        } else {
+            spaceGame.getBatch().draw(buttonHomeIdle, xHome, yHome, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            isCheckSoundButtonHomeOn = false;
+        }
+        // how to play
+        spaceGame.getBatch().draw(howToPlay,125,100,550,600);
+        // button music
+        int x = 0;
+        int y = 0;
+
+        Vector3 touchPointMusic = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPointMusic); // chuyển tỏa độ
+        boolean isTouchingButtonMusic = touchPointMusic.x > x && touchPointMusic.x < x + SIZE_BUTTON_MUSIC_WIDTH &&
+                touchPointMusic.y > y && touchPointMusic.y < y + SIZE_BUTTON_MUSIC_HEIGHT;
+        if (isTouchingButtonMusic) {
+            if(checkSoundOn) spaceGame.getBatch().draw(buttonMusicOnHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            else spaceGame.getBatch().draw(buttonMusicOffHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+
+                if(checkSoundOn) {
+
+                    //spaceGame.getBatch().draw(buttonMusicOffHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+                    checkSoundOn = false;
+                    backgroundMusic.pause();
+                }
+                else {
+                    //spaceGame.getBatch().draw(buttonMusicOnHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+                    backgroundMusic.play();
+                    checkSoundOn = true;
+                }
+            }
+            if(checkSoundOn){
+                if(!checkSoundButtonMusicOffOn){
+                    clickButtonMusic.play();
+                    checkSoundButtonMusicOffOn = true;
+                }
+            }
+
+        } else {
+            if(checkSoundOn) spaceGame.getBatch().draw(buttonMusicOnIdle, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            else spaceGame.getBatch().draw(buttonMusicOffIdle, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            checkSoundButtonMusicOffOn = false;
+
+
+        }
+
+        spaceGame.getBatch().end();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/com/mygdx/game/view/MenuScreen.java
+++ core/src/com/mygdx/game/view/MenuScreen.java
@@ -3,228 +3,223 @@ package com.mygdx.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.SpaceGame;
-import com.mygdx.game.model.gamemusic.MusicGame;
-import com.mygdx.game.model.gamemusic.MusicHandler;
-import com.mygdx.game.setting.Media_Menu;
-import com.mygdx.game.setting.Setting_MenuScreen;
+
+import java.util.PrimitiveIterator;
 
 public class MenuScreen implements Screen {
-    private SpaceGame game;
-    public final Stage stage;
-    TextButton buttonRecordFunction;
-    TextButton buttonNetwork;
-    TextButton buttonLevel1;
-    TextButton buttonLevel2;
-    TextButton buttonMenu;
-    TextButton buttonNewGame;
-    TextButton buttonOldGame;
-    TextButton buttonExit;
-    Media_Menu mediaMenu;
-    MusicHandler musicHandler;
-    MusicGame background_Menu_Music, clickButton_Menu_Music, open_Game_Music;
-    public MenuScreen(final SpaceGame game){
-        this.game = game;
-        this.stage = new Stage(new FillViewport(Setting_MenuScreen.WINDOWS_SIZE, Setting_MenuScreen.WINDOWS_SIZE));
-        this.musicHandler = new MusicHandler();
-        this.setMusic();
-        mediaMenu = new Media_Menu();
-        mediaMenu.loadAssets();
+    // KÍCH THƯỚC NÚT
+    private int SIZE_BUTTON_WIDTH = 88;
+    private int SIZE_BUTTON_HEIGHT = 88;
+    private int SIZE_BUTTON_MUSIC_WIDTH = SIZE_BUTTON_WIDTH/2;
+    private int SIZE_BUTTON_MUSIC_HEIGHT = SIZE_BUTTON_HEIGHT/2;
+    // CÁC CHỨC NĂNG KHÁC
+    SpaceGame spaceGame;
+    private OrthographicCamera camera;
+
+    // ÂM THANH
+    private Music backgroundMusic;
+    private Music clickButtonMusic;
+
+    // HÌNH ẢNH
+    Texture background;
+    Texture buttonNewGameIdle;
+    Texture buttonNewGameHover;
+    Texture buttonInforGameIdle;
+    Texture buttonInforGameHover;
+    Texture buttonRankGameIdle;
+    Texture buttonRankGameHover;
+    Texture buttonMusicOnIdle;
+    Texture buttonMusicOnHover;
+    Texture buttonMusicOffIdle;
+    Texture buttonMusicOffHover;
+
+    // CHỨC NĂNG CHECK ÂM THANH
+    boolean checkSoundButtonPlayOn = false;
+    boolean checkSoundButtonInforOn = false;
+    boolean checkSoundButtonRankOn = false;
+    boolean checkSoundButtonMusicOffOn = false;
+    static boolean checkSoundOn = true;
+
+    public MenuScreen(SpaceGame spaceGame){
+        this.spaceGame = spaceGame;
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Menu_Music/watery-graves-181198.mp3"));
+        clickButtonMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Menu_Music/clickButton.mp3"));
+        background = new Texture("button/Background.png");
+        buttonNewGameIdle = new Texture("button/Play-Idle.png");
+        buttonNewGameHover = new Texture("button/Play-Hover.png");
+        buttonInforGameIdle = new Texture("button/Info-Idle.png");
+        buttonInforGameHover = new Texture("button/Info-Hover.png");
+        buttonRankGameIdle = new Texture("button/Star-Idle.png");
+        buttonRankGameHover = new Texture("button/Star-Hover.png");
+        buttonMusicOnIdle = new Texture("button/Music-On-Idle.png");
+        buttonMusicOnHover = new Texture("button/Music-On-Hover.png");
+        buttonMusicOffIdle = new Texture("button/Music-Off-Idle.png");
+        buttonMusicOffHover = new Texture("button/Music-Off-Hover.png");
+
     }
-    public void setMusic(){
-        this.background_Menu_Music = new MusicGame(musicHandler.background_Menu,true);
-        this.clickButton_Menu_Music = new MusicGame(musicHandler.clickbutton_Menu, false);
-        this.open_Game_Music = new MusicGame(musicHandler.open_Game, false);
-        this.background_Menu_Music.setPlay();
+    public void show(){
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+        if(checkSoundOn) backgroundMusic.play();
     }
-    @Override
-    public void show() {
-        Gdx.input.setInputProcessor(stage);
-        stage.clear();
-        initButtons();
-    }
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(delta);
-        stage.getBatch().begin();
-        stage.getBatch().draw(mediaMenu.imgMenu, 0,0);
-        stage.getBatch().end();
-        stage.draw();
-    }
-    @Override
-    public void resize(int width, int height) {}
-    @Override
-    public void pause() {}
-    @Override
-    public void resume() {}
-    @Override
-    public void hide() {}
-    @Override
-    public void dispose() {
-        stage.dispose();
-        mediaMenu.disposeAssets();
-    }
-    private void initButtons() {
-        buttonMenu = new TextButton("Back to Menu", Setting_MenuScreen.skin, "default");
-        buttonMenu.setPosition((float) Setting_MenuScreen.WINDOWS_SIZE / 2 - 140, (float) Setting_MenuScreen.WINDOWS_SIZE / 2 + 20);
-        buttonMenu.setSize(Setting_MenuScreen.BUTTON_WIDTH, Setting_MenuScreen.BUTTON_HEIGHT);
-        buttonMenu.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                clickButton_Menu_Music.setPlay();
-                // Xóa màn hình, thêm các nút của menu
-                stage.clear();
-                //stage.addActor(buttonRecordFunction);
-                //stage.addActor(buttonNetwork);
-                stage.addActor(buttonNewGame);
-                // stage.addActor(buttonOldGame);
-                stage.addActor(buttonExit);
-                // mediaMenu.buttonSound.play();
+
+    public void render(float var1){
+        Gdx.gl.glClearColor(1, 1, 1, 0);
+        ScreenUtils.clear(1, 1, 1, 0);
+
+        spaceGame.getBatch().begin();
+        spaceGame.getBatch().draw(background, 0, 0, 800, 800);
+        // button music
+        int x = 0;
+        int y = 0;
+
+        Vector3 touchPointMusic = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPointMusic); // chuyển tỏa độ
+        boolean isTouchingButtonMusic = touchPointMusic.x > x && touchPointMusic.x < x + SIZE_BUTTON_MUSIC_WIDTH &&
+                touchPointMusic.y > y && touchPointMusic.y < y + SIZE_BUTTON_MUSIC_HEIGHT;
+        if (isTouchingButtonMusic) {
+            if(checkSoundOn) spaceGame.getBatch().draw(buttonMusicOnHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            else spaceGame.getBatch().draw(buttonMusicOffHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+
+                if(checkSoundOn) {
+
+                    //spaceGame.getBatch().draw(buttonMusicOffHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+                    checkSoundOn = false;
+                    backgroundMusic.pause();
+                }
+                else {
+                    //spaceGame.getBatch().draw(buttonMusicOnHover, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+                    backgroundMusic.play();
+                    checkSoundOn = true;
+                }
             }
-        });
-
-        buttonLevel1 = new TextButton("Level 1", Setting_MenuScreen.skin, "default");
-        buttonLevel1.setPosition((float) Setting_MenuScreen.WINDOWS_SIZE / 2 - 300, (float) Setting_MenuScreen.WINDOWS_SIZE / 2 + 120);
-        buttonLevel1.setSize(Setting_MenuScreen.BUTTON_WIDTH, Setting_MenuScreen.BUTTON_HEIGHT);
-        buttonLevel1.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                clickButton_Menu_Music.setPlay();
-                background_Menu_Music.setStop();
-                open_Game_Music.setPlay();
-                //System.out.println("Chọn ải 1.");
-                dispose();
-                game.setScreen(new GameScreen(game));
+            if(checkSoundOn){
+                if(!checkSoundButtonMusicOffOn){
+                    clickButtonMusic.play();
+                    checkSoundButtonMusicOffOn = true;
+                }
             }
-        });
 
-//        buttonLevel2 = new TextButton("Level 2", Setting_MenuScreen.skin, "default");
-//        buttonLevel2.setPosition((float) Setting_MenuScreen.WINDOWS_SIZE / 2 + 20, (float) Setting_MenuScreen.WINDOWS_SIZE / 2 + 120);
-//        buttonLevel2.setSize(Setting_MenuScreen.BUTTON_WIDTH, Setting_MenuScreen.BUTTON_HEIGHT);
-//        buttonLevel2.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                System.out.println("Chọn ải 2.");
-//              //  dispose();
-//                //game.setScreen(new GamePlayingScreen(game, Variable.LEVEL_2));
-//            }
-//        });
+        } else {
+            if(checkSoundOn) spaceGame.getBatch().draw(buttonMusicOnIdle, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            else spaceGame.getBatch().draw(buttonMusicOffIdle, x, y, SIZE_BUTTON_MUSIC_WIDTH, SIZE_BUTTON_MUSIC_HEIGHT);
+            checkSoundButtonMusicOffOn = false;
 
-        buttonNewGame = new TextButton("New Game", Setting_MenuScreen.skin, "default");
-        buttonNewGame.setPosition((float) Setting_MenuScreen.WINDOWS_SIZE / 2 - 300, (float) Setting_MenuScreen.WINDOWS_SIZE / 2 + 120);
-        buttonNewGame.setSize(Setting_MenuScreen.BUTTON_WIDTH, Setting_MenuScreen.BUTTON_HEIGHT);
-        buttonNewGame.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                // Xóa màn hình, thêm các nút chọn ải.
-                clickButton_Menu_Music.setPlay();
-                stage.clear();
-                stage.addActor(buttonLevel1);
-                //stage.addActor(buttonLevel2);
-                stage.addActor(buttonMenu);
-                // mediaMenu.buttonSound.play();
+
+        }
+        // Button play game
+
+        int xPlay = 220 + SIZE_BUTTON_WIDTH + 40; // tọa độ x của nút play
+        int yPlay = 425; // tọa độ y của nút play
+
+        Vector3 touchPointPlay = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPointPlay); // Chuyển đổi tọa độ
+
+        boolean isTouchingButtonPlay = touchPointPlay.x > xPlay && touchPointPlay.x < xPlay + SIZE_BUTTON_WIDTH &&
+                touchPointPlay.y > yPlay && touchPointPlay.y < yPlay + SIZE_BUTTON_HEIGHT;
+
+        if (isTouchingButtonPlay) {
+            spaceGame.getBatch().draw(buttonNewGameHover, xPlay, yPlay, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                backgroundMusic.pause();
+                spaceGame.setScreen(new GameScreen(spaceGame));
             }
-        });
-
-//        buttonOldGame = new TextButton("Old Game", Variable.skin, "default");
-//        buttonOldGame.setPosition((float) Variable.WINDOWS_SIZE / 2 + 20, (float) Variable.WINDOWS_SIZE / 2 + 120);
-//        buttonOldGame.setSize(Variable.BUTTON_WIDTH, Variable.BUTTON_HEIGHT);
-//        buttonOldGame.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                stage.clear();
-//                if(new File(Variable.GAME_DATA_1).exists()){
-//                    TextButton buttonData1;
-//                    buttonData1 = new TextButton("Save 1 :)", Variable.skin, "default");
-//                    buttonData1.setPosition((float) Variable.WINDOWS_SIZE / 2 - 300, (float) Variable.WINDOWS_SIZE / 2 + 120);
-//                    buttonData1.setSize(Variable.BUTTON_WIDTH, Variable.BUTTON_HEIGHT);
-//                    buttonData1.addListener(new ClickListener() {
-//                        @Override
-//                        public void clicked(InputEvent event, float x, float y) {
-//                            dispose();
-//                            LoadGameDataTool loadTool = new LoadGameDataTool(game);
-//                            try {
-//                                loadTool.LoadFromFile(Variable.GAME_DATA_1);
-//                            } catch (IOException e) {
-//                                throw new RuntimeException(e);
-//                            }
-//                            game.setScreen(loadTool.getGamePlayingScreen());
-//                        }
-//                    });
-//                    stage.addActor(buttonData1);
-//                }
-//                else{
-//                    TextButton buttonEmpty;
-//                    buttonEmpty = new TextButton("Empty :(", Variable.skin, "default");
-//                    buttonEmpty.setPosition((float) Variable.WINDOWS_SIZE / 2 - 300, (float) Variable.WINDOWS_SIZE / 2 + 120);
-//                    buttonEmpty.setSize(Variable.BUTTON_WIDTH, Variable.BUTTON_HEIGHT);
-//                    buttonEmpty.addListener(new ClickListener() {
-//                        @Override
-//                        public void clicked(InputEvent event, float x, float y) {
-//                            MediaMenu.buttonSound.play();
-//                        }
-//                    });
-//                    stage.addActor(buttonEmpty);
-//                }
-//                if(Gdx.files.internal(Variable.GAME_DATA_2).exists()){
-//                    TextButton buttonData2;
-//                    buttonData2 = new TextButton("Save 2 :)", Variable.skin, "default");
-//                    buttonData2.setPosition((float) Variable.WINDOWS_SIZE / 2 + 20, (float) Variable.WINDOWS_SIZE / 2 + 120);
-//                    buttonData2.setSize(Variable.BUTTON_WIDTH, Variable.BUTTON_HEIGHT);
-//                    buttonData2.addListener(new ClickListener() {
-//                        @Override
-//                        public void clicked(InputEvent event, float x, float y) {
-//                            dispose();
-//                            LoadGameDataTool loadTool = new LoadGameDataTool(game);
-//                            try {
-//                                loadTool.LoadFromFile(Variable.GAME_DATA_2);
-//                            } catch (IOException e) {
-//                                throw new RuntimeException(e);
-//                            }
-//                            game.setScreen(loadTool.getGamePlayingScreen());
-//                        }
-//                    });
-//                    stage.addActor(buttonData2);
-//                }
-//                else{
-//                    TextButton buttonEmpty;
-//                    buttonEmpty = new TextButton("Empty :(", Variable.skin, "default");
-//                    buttonEmpty.setPosition((float) Variable.WINDOWS_SIZE / 2 + 20, (float) Variable.WINDOWS_SIZE / 2 + 120);
-//                    buttonEmpty.setSize(Variable.BUTTON_WIDTH, Variable.BUTTON_HEIGHT);
-//                    buttonEmpty.addListener(new ClickListener() {
-//                        @Override
-//                        public void clicked(InputEvent event, float x, float y) {
-//                            MediaMenu.buttonSound.play();
-//                        }
-//                    });
-//                    stage.addActor(buttonEmpty);
-//                }
-//                stage.addActor(buttonMenu);
-//                MediaMenu.buttonSound.play();
-//            }
-//        });
-
-        buttonExit = new TextButton("Exit", Setting_MenuScreen.skin, "default");
-        buttonExit.setPosition((float) Setting_MenuScreen.WINDOWS_SIZE /2 - 140, (float) Setting_MenuScreen.WINDOWS_SIZE / 2 + 20);
-        buttonExit.setSize(Setting_MenuScreen.BUTTON_WIDTH, Setting_MenuScreen.BUTTON_HEIGHT);
-        buttonExit.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.app.exit();
+            if(!checkSoundButtonPlayOn && checkSoundOn){
+                checkSoundButtonPlayOn = true;
+                clickButtonMusic.play();
             }
-        });
 
-        //stage.addActor(buttonRecordFunction);
-        //stage.addActor(buttonNetwork);
-        stage.addActor(buttonNewGame);
-        //stage.addActor(buttonOldGame);
-        stage.addActor(buttonExit);
+        } else {
+            spaceGame.getBatch().draw(buttonNewGameIdle, xPlay, yPlay, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            checkSoundButtonPlayOn = false;
+        }
+
+
+        // Button infor game
+        int xInfor = 220; // tọa độ x của nút infor
+        int yInfor = 425; // tọa độ y của nút infor
+
+        Vector3 touchPointInfor = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPointInfor); // Chuyển đổi tọa độ
+
+        boolean isTouchingButtonInfor = touchPointInfor.x > xInfor && touchPointInfor.x < xInfor + SIZE_BUTTON_WIDTH &&
+                touchPointInfor.y > yInfor && touchPointInfor.y < yInfor + SIZE_BUTTON_HEIGHT;
+
+        if (isTouchingButtonInfor) {
+            spaceGame.getBatch().draw(buttonInforGameHover, xInfor, yInfor, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                clickButtonMusic.pause();
+                backgroundMusic.pause();
+                spaceGame.setScreen(new InforGameScreen(spaceGame));
+            }
+            if(!checkSoundButtonInforOn && checkSoundOn){
+                checkSoundButtonInforOn = true;
+                clickButtonMusic.play();
+            }
+
+        } else {
+            spaceGame.getBatch().draw(buttonInforGameIdle, xInfor, yInfor, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            checkSoundButtonInforOn = false;
+        }
+
+        // Button rank game
+        int xRank = 220 + SIZE_BUTTON_WIDTH + 40 + SIZE_BUTTON_WIDTH + 40; // tọa độ x của nút rank
+        int yRank = 425; // tọa độ y của nút rank
+
+        Vector3 touchPointRank = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPointRank); // Chuyển đổi tọa độ
+
+        boolean isTouchingButtonRank = touchPointInfor.x > xRank && touchPointInfor.x < xRank + SIZE_BUTTON_WIDTH &&
+                touchPointInfor.y > yRank && touchPointInfor.y < yRank + SIZE_BUTTON_HEIGHT;
+
+        if (isTouchingButtonRank) {
+            spaceGame.getBatch().draw(buttonRankGameHover, xRank, yRank, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                //spaceGame.setScreen(new GameScreen(spaceGame));
+            }
+            if(!checkSoundButtonRankOn && checkSoundOn){
+                checkSoundButtonRankOn = true;
+                clickButtonMusic.play();
+            }
+
+        } else {
+            spaceGame.getBatch().draw(buttonRankGameIdle, xRank, yRank, SIZE_BUTTON_WIDTH, SIZE_BUTTON_HEIGHT);
+            checkSoundButtonRankOn = false;
+        }
+
+
+        spaceGame.getBatch().end();
+    }
+
+    public void resize(int var1, int var2){
+
+    }
+
+    public void pause(){
+
+    }
+
+    public void resume(){
+
+    }
+
+    public void hide(){
+
+    }
+
+    public void dispose(){
+
     }
 }
